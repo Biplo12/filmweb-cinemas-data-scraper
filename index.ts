512d861a58da96302c01fcba07dd30810acfcf95
Robert Siński <robert.sinski@outlook.com>
2024-08-16 14:35:44 +0200

    refactor: Refactor code to use extractTextContentFromSelector instead of extractMultipleTextContentFromSelectors

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import {
   capitalizeFirstLetter,
   durationStringToNumber,
-  extractMultipleTextContentFromSelectors,
+  extractTextContentFromSelector,
 } from "./lib/utils";
 import { JSDOM } from "jsdom";
 
@@ -22,68 +22,58 @@ const parseHtmlToDocument = (html: string): Document => {
 };
 
 const extractMovieData = (document: Document) => {
+  const movieCards = document.querySelectorAll(
+    "div.showtimesFilmsItem__filmPreview.previewHolder.isSmall.smMicro.isBold.showGenresInHeader.showDuration.noPadding.noYear.variantPlot.variantAdditionalInfo"
+  );
+
   const selectors = [
     { key: "title", selector: "a.preview__link" },
     { key: "year", selector: "div.preview__year" },
     { key: "duration", selector: "div.preview__duration" },
     { key: "imagePoster", selector: "img.poster__image", attribute: "src" },
-    {
-      key: "director",
-      selector: "div.preview__detail--director h3 a span",
-    },
+    { key: "director", selector: "div.preview__detail--director h3 a span" },
     { key: "description", selector: "div.preview__plotText" },
     { key: "communityRating", selector: "span.communityRatings__value" },
     { key: "criticsRating", selector: "span.communityRatings__value.isHigh" },
   ];
 
-  const extractedData = selectors.reduce(
-    (acc, { key, selector, attribute }) => {
-      acc[key] = extractMultipleTextContentFromSelectors(
-        selector,
-        document,
-        attribute
-      ) as string[];
-      return acc;
-    },
-    {} as Record<string, any>
-  );
+  const films = Array.from(movieCards).map((card) => {
+    const film: Record<string, any> = {};
 
-  const dateSelected = extractMultipleTextContentFromSelectors(
+    selectors.forEach(({ key, selector, attribute }) => {
+      film[key] = extractTextContentFromSelector(selector, card, attribute);
+    });
+
+    if (film["duration"]) {
+      film["durationInMinutes"] = durationStringToNumber(film["duration"]);
+    }
+
+    return film;
+  });
+
+  const dateSelected = extractTextContentFromSelector(
     "li.navList__item--selected",
     document,
     "data-value"
   );
-
-  const films = extractedData.title.map((_: any, index: number) => {
-    const film: Record<string, any> = {};
-    for (const key in extractedData) {
-      film[key] = extractedData[key][index] || "N/A";
-
-      if (key === "duration") {
-        film["durationInMinutes"] = durationStringToNumber(
-          film[key as keyof typeof film]
-        );
-      }
-    }
-    return film;
-  });
 
   return {
     date: dateSelected || "N/A",
     films,
   };
 };
-
 const main = async () => {
   try {
     const html = await fetchMoviesPage("Bytom");
     const document = parseHtmlToDocument(html);
     const data = extractMovieData(document);
 
-    console.log("Filtered Data:", JSON.stringify(data.films[10], null, 2));
+    console.log("Filtered Data:", JSON.stringify(data, null, 2));
   } catch (error) {
     console.error("Error fetching or processing data:", error);
   }
 };
+
+main();
 
 main();
--- lib/utils.ts
+++ lib/utils.ts
@@ -3,27 +3,6 @@ export const capitalizeFirstLetter = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
-// Helper function to extract text content from a query selector
-export const extractMultipleTextContentFromSelectors = (
-  selector: string,
-  document: Document,
-  attribute?: string
-): string | string[] => {
-  const texts = Array.from(document.querySelectorAll(selector)).map((el) => {
-    if (attribute) {
-      return (el as HTMLElement).getAttribute(attribute)?.trim() || "";
-    }
-    return el.textContent?.trim() || "";
-  });
-
-  const filteredTexts = texts.filter((text) => text !== "");
-
-  if (filteredTexts.length === 1) {
-    return filteredTexts[0];
-  }
-  return filteredTexts;
-};
-
 export const durationStringToNumber = (duration: string): number => {
   const parts = duration.split(" ");
   let totalMinutes = 0;
@@ -37,4 +16,19 @@ export const durationStringToNumber = (duration: string): number => {
   });
 
   return totalMinutes;
+};
+
+export const extractTextContentFromSelector = (
+  selector: string,
+  element: Element | Document,
+  attribute?: string
+): string => {
+  const selectedElement = element.querySelector(selector);
+  if (!selectedElement) return "N/A";
+
+  if (attribute) {
+    return selectedElement.getAttribute(attribute) || "N/A";
+  }
+
+  return selectedElement.textContent?.trim() || "N/A";
 };
